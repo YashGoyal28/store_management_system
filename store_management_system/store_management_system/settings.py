23d32a450341d8d37392ed8b79181004cebab30f
YashGoyal28 <y.g.2001yashgoyal@gmail.com>
2020-11-02 11:19:44 +0530

    required

--- store_management_system/store_management_system/settings.py
+++ store_management_system/store_management_system/settings.py
@@ -78,8 +78,12 @@ WSGI_APPLICATION = 'store_management_system.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'ITW',
+        'HOST': 'localhost',
+        'USER': 'root',
+        'PORT': '3306',
+        'PASSWORD': 'MyNewPass',
     }
 }
 
